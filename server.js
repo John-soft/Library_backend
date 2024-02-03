f08b71fb0c0604046355386c5f47aadf29365ed6
John-soft <akinwalere52@gmail.com>
2024-02-03 17:05:01 +0100

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dbConnect = require("./db");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
-dbConnect(process.env.MONGO_URI);
+dbConnect(process.env.CONN_STR);
 
 app.listen(PORT, "localhost", () => {
   console.log(`Server running on port ${PORT}`);
