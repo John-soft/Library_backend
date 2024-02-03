3219ea04b635f299f7fae519ac5d3fda75aea364
John-soft <akinwalere52@gmail.com>
2024-02-03 17:34:04 +0100

    changes update

---
+++ config/token.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return (
+    jwt.sign({ id }, process.env.JWT_STR),
+    {
+      expiresIn: "1d",
+    }
+  );
+};
+
+module.exports = generateToken;
