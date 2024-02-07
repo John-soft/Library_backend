004b694cd0fc79ce621c586fd8a2ee2a61ec86f8
John-soft <akinwalere52@gmail.com>
2024-02-07 09:10:42 +0100

    db string

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const dbConnect = require("./utils/db");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
-dbConnect(process.env.CONN_STR);
+dbConnect(
+  "mongodb+srv://admin:gHR3RTd3M6zT2d52@cluster0.a4dxzy6.mongodb.net/library-backend?retryWrites=true&w=majority"
+);
 
 app.listen(PORT, "localhost", () => {
   console.log(`Server running on port ${PORT}`);
